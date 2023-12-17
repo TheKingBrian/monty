9187f3be7bdd0e92a2c08fb6828a59e3b2582d3a
TheKingBrian <brian121molope@gmail.com>
2023-12-17 14:41:20 -0800

    updated

--- pchar.c
+++ pchar.c
@@ -3,25 +3,27 @@
 #include <stdlib.h>
 #include <string.h>
 #include "monty.h"
+
 /**
- * pchar - shows all the char
+ * pchar - prints the int at the top of the stack as char
  * @stack: stack given by main
  * @line_cnt: ammount of lines
- * Return: gone
+ *
+ * Return: void
  */
 void pchar(stack_t **stack, unsigned int line_cnt)
 {
-if (!stack || !(*stack))
-{
-fprintf(stderr, "L%d: can't pchar, stack empty\n", line_cnt);
-status = EXIT_FAILURE;
-return;
-}
-if (isascii((*stack)->n) == 0)
-{
-fprintf(stderr, "L%d: can't pchar, value out of range\n", line_cnt);
-status = EXIT_FAILURE;
-return;
-}
-printf("%c\n", (*stack)->n);
+	if (!stack || !(*stack))
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_cnt);
+		status = EXIT_FAILURE;
+		return;
+	}
+	if (isascii((*stack)->n) == 0)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_cnt);
+		status = EXIT_FAILURE;
+		return;
+	}
+	printf("%c\n", (*stack)->n);
 }
